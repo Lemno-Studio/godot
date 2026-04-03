539a1567c379360a65e5cc565887cb4d567d173f
dalmurii <mincraft20046@gmail.com>
2026-04-04 00:36:12 +0900

    DRAFT
     Changes to be committed:
    	modified:   install-thrdparty-windows.bat

--- install-thrdparty-windows.bat
+++ install-thrdparty-windows.bat
@@ -3,7 +3,7 @@ python -m pip install scons
 
 curl -L -o cppwinrt.zip https://www.nuget.org/api/v2/package/Microsoft.Windows.CppWinRT/2.0.190401.2
 mkdir	.\.tmp
-tar -xf cppwinrt.zip .\.tmp
+tar -xf cppwinrt.zip -C .\.tmp
 .\.tmp\bin\cppwinrt.exe -in local -out .\platform\windows -verbose
 rmdir /s /q .\.tmp
 
