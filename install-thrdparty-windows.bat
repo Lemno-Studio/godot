f45f2ff0a839cb05f1696b5cbdfd874c25109ca6
dalmurii <mincraft20046@gmail.com>
2026-04-04 00:34:24 +0900

    DRAFT
     Changes to be committed:
    	modified:   install-thrdparty-windows.bat

--- install-thrdparty-windows.bat
+++ install-thrdparty-windows.bat
@@ -2,6 +2,7 @@ winget install Microsoft.VisualStudio.2022.BuildTools
 python -m pip install scons
 
 curl -L -o cppwinrt.zip https://www.nuget.org/api/v2/package/Microsoft.Windows.CppWinRT/2.0.190401.2
+mkdir	.\.tmp
 tar -xf cppwinrt.zip .\.tmp
 .\.tmp\bin\cppwinrt.exe -in local -out .\platform\windows -verbose
 rmdir /s /q .\.tmp
