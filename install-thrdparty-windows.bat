3473945e9e51f949d269ed902ac704566a86b030
dalmurii <mincraft20046@gmail.com>
2026-04-04 00:23:16 +0900

    FIX TYPO
     Changes to be committed:
    	modified:   install-thrdparty-windows.bat

--- install-thrdparty-windows.bat
+++ install-thrdparty-windows.bat
@@ -4,6 +4,6 @@ python -m pip install scons
 curl -o cppwinrt.zip https://www.nuget.org/api/v2/package/Microsoft.Windows.CppWinRT/2.0.190401.2
 tar -xf cppwinrt.zip .\.tmp
 tar -xf .\.tmp\bin\cppwinrt.exe -in local -out .\platform\windows -verbose
-rmdir /s /q .\tmp
+rmdir /s /q .\.tmp
 
 echo "DONE. Restart your terminal"
