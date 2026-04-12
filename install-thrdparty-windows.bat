1725125bffea45bcc0e322fb0261b370e64f763a
dalmurii <mincraft20046@gmail.com>
2026-04-12 20:02:34 +0900

    FIX: install script
     Changes to be committed:
    	- modified:   install-thrdparty-windows.bat

--- install-thrdparty-windows.bat
+++ install-thrdparty-windows.bat
@@ -1,7 +1,7 @@
 winget install BrechtSanders.WinLibs.POSIX.UCRT.LLVM
 winget install python
 
-
-python -m pip install scons
+python -m venv .\.venv
+.\.venv\Scripts\pip install scons
 
 echo "DONE. Restart your terminal"
