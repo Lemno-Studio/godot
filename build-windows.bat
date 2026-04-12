ee167f1c07b3ba8ef34387c59d24737003e7f612
dalmurii <mincraft20046@gmail.com>
2026-04-12 20:00:04 +0900

    UPDATE: install/build calls
     Changes to be committed:
    	- modified:   build-windows.bat
    	- modified:   install-thrdparty-windows.bat

--- build-windows.bat
+++ build-windows.bat
@@ -1,2 +1,10 @@
+mkdir	.\.tmp
+
+curl -L -o .\.tmp\cppwinrt.zip https://www.nuget.org/api/v2/package/Microsoft.Windows.CppWinRT/2.0.190401.2
+tar -xf .\.tmp\cppwinrt.zip -C .\.tmp
+.\.tmp\bin\cppwinrt.exe -in local -out .\platform\windows -verbose
+
+rmdir /s /q .\.tmp
+
 cmake -Bbuild -GNinja
-.\.venv\Scripts\scons.exe d3d12=no angle=no
+.\.venv\Scripts\scons.exe d3d12=no angle=no use_llvm=yes use_mingw=yes
--- install-thrdparty-windows.bat
+++ install-thrdparty-windows.bat
@@ -1,15 +1,7 @@
 winget install BrechtSanders.WinLibs.POSIX.UCRT.LLVM
-winget install phthon
+winget install python
 
 
 python -m pip install scons
-
-mkdir	.\.tmp
-
-curl -L -o .\.tmp\cppwinrt.zip https://www.nuget.org/api/v2/package/Microsoft.Windows.CppWinRT/2.0.190401.2
-tar -xf .\.tmp\cppwinrt.zip -C .\.tmp
-.\.tmp\bin\cppwinrt.exe -in local -out .\platform\windows -verbose
-
-rmdir /s /q .\.tmp
 
 echo "DONE. Restart your terminal"
